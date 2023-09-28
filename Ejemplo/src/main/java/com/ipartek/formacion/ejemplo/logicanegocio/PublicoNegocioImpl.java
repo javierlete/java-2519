24e573685f1e5cdf7418b0ce291966c17d4ce8cd
java <java@DESKTOP-MHK683Q.mshome.net>
2023-09-28 14:24:34 +0200

    Implementación del negocio

---
+++ Ejemplo/src/main/java/com/ipartek/formacion/ejemplo/logicanegocio/PublicoNegocioImpl.java
@@ -0,0 +1,19 @@
+package com.ipartek.formacion.ejemplo.logicanegocio;
+
+import com.ipartek.formacion.ejemplo.accesodatos.ProductoDao;
+import com.ipartek.formacion.ejemplo.accesodatos.ProductoDaoSqlite;
+import com.ipartek.formacion.ejemplo.entidades.Producto;
+
+import lombok.extern.java.Log;
+
+@Log
+public class PublicoNegocioImpl implements PublicoNegocio {
+	private ProductoDao daoProducto = new ProductoDaoSqlite(); 
+	
+	@Override
+	public Iterable<Producto> obtenerProductos() {
+		log.info("Se han pedido todos los productos");
+		return daoProducto.selectTodos();
+	}
+
+}
