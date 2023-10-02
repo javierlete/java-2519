c584f6dea18b56dd39101821cd6deb8ad1913795
java <java@DESKTOP-MHK683Q.mshome.net>
2023-10-02 10:28:29 +0200

    Utilización de Java Validation en la entidad

--- Ejemplo/src/main/java/com/ipartek/formacion/ejemplo/entidades/Producto.java
+++ Ejemplo/src/main/java/com/ipartek/formacion/ejemplo/entidades/Producto.java
@@ -5,6 +5,11 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +20,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class Producto {
+	@Min(0)
 	private Long id;
+	
+	@NotNull
+	@Size(min = 3)
+	@Pattern(regexp="^[A-Z][a-z ]*$")
 	private String nombre;
+	
+	@NotNull
+	@Min(0)
 	private BigDecimal precio;
+
+	@Future
 	@JsonFormat(pattern = "yyyy-MM-dd")
 	private LocalDate fechaCaducidad;
 }
