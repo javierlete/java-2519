75cb6c381f249e2cdee01a417605667e8cd85327
java <java@DESKTOP-MHK683Q.mshome.net>
2023-10-02 11:02:08 +0200

    Implementamos llamadas en JavaScript al servicio REST

--- Ejemplo/src/main/webapp/js/clienterest.js
+++ Ejemplo/src/main/webapp/js/clienterest.js
@@ -1,12 +1,42 @@
+'use strict';
+
+const URL = 'http://localhost:8080/ejemplo/api/productos';
+
+let ul;
+
 window.addEventListener('DOMContentLoaded', async function() {
-	const ul = document.querySelector('ul');
+	ul = document.querySelector('ul');
 	
-	const respuesta = await fetch('http://localhost:8080/ejemplo/api/productos');
+	await cargarProductos();
+
+	const form = document.querySelector('form');
+	const button = document.querySelector('form button');
+	button.addEventListener('click', async function() {
+		const producto = { nombre: form.nombre.value, precio: form.precio.value, fechaCaducidad: form['fecha-caducidad'].value };
+
+		const respuesta = await fetch(URL, {
+			method: 'POST',
+			body: JSON.stringify(producto),
+		});
+
+		const datos = await respuesta.json();
+
+		console.log(datos);
+
+		cargarProductos();
+	});
+
+});
+
+async function cargarProductos() {
+	const respuesta = await fetch(URL);
 	const productos = await respuesta.json();
-	
+
+	ul.innerHTML = '';
+
 	productos.forEach(p => {
 		const li = document.createElement('li');
 		li.innerHTML = `${p.id}, ${p.nombre}, ${p.precio}, ${p.fechaCaducidad}`;
-		ul.appendChild(li);	
+		ul.appendChild(li);
 	});
-});
+}
