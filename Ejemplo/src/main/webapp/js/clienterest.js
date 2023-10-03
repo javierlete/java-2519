552420024a90534b89a9fa94c1e6357104f3fd84
java <java@DESKTOP-MHK683Q.mshome.net>
2023-10-03 10:01:26 +0200

    Cliente REST con Bootstrap y DataTables

--- Ejemplo/src/main/webapp/js/clienterest.js
+++ Ejemplo/src/main/webapp/js/clienterest.js
@@ -2,16 +2,30 @@
 
 const URL = 'http://localhost:8080/ejemplo/api/productos';
 
-let ul;
+let tbody;
+let dataTable;
 
 window.addEventListener('DOMContentLoaded', async function() {
-	ul = document.querySelector('ul');
+	tbody = document.querySelector('tbody');
+	const inputFechaCaducidad = document.querySelector('#fecha-caducidad');
 	
+	const minimo = new Date();
+	minimo.setDate(minimo.getDate() + 1);
+	
+	inputFechaCaducidad.min = minimo.toISOString().split('T')[0]; 
+
 	await cargarProductos();
 
 	const form = document.querySelector('form');
-	const button = document.querySelector('form button');
-	button.addEventListener('click', async function() {
+	
+	form.addEventListener('submit', async function(e) {
+		e.preventDefault();
+		
+		if(!form.checkValidity()) {
+			form.classList.add('was-validated');
+			return;
+		}
+		
 		const producto = { nombre: form.nombre.value, precio: form.precio.value, fechaCaducidad: form['fecha-caducidad'].value };
 
 		const respuesta = await fetch(URL, {
@@ -32,11 +46,28 @@ async function cargarProductos() {
 	const respuesta = await fetch(URL);
 	const productos = await respuesta.json();
 
-	ul.innerHTML = '';
+	dataTable && dataTable.destroy();
+	
+	tbody.innerHTML = '';
 
 	productos.forEach(p => {
-		const li = document.createElement('li');
-		li.innerHTML = `${p.id}, ${p.nombre}, ${p.precio}, ${p.fechaCaducidad}`;
-		ul.appendChild(li);
+		const tr = document.createElement('tr');
+		tr.innerHTML = `
+			<th>${p.id}</th>
+			<td>${p.nombre}</td>
+			<td>${p.precio}€</td>
+			<td>${p.fechaCaducidad}</td>
+			<td><a class="btn btn-primary" href="#">Editar</a> <a
+				class="btn btn-danger" href="#">Borrar</a></td>
+		`;
+		tbody.appendChild(tr);
+	});
+
+	dataTable = $('table').DataTable({
+		pageLength: 4,
+		lengthMenu: [4, 10, 25, 50, 75, 100],
+		language: {
+			url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json',
+		}
 	});
 }
