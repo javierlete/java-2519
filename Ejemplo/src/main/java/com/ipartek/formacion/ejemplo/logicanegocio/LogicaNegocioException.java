2f6386a0cbcb579a1da5f33823e794f9b217f044
java <java@DESKTOP-MHK683Q.mshome.net>
2023-10-03 10:01:15 +0200

    Lógica de negocio valida el producto a añadir

---
+++ Ejemplo/src/main/java/com/ipartek/formacion/ejemplo/logicanegocio/LogicaNegocioException.java
@@ -0,0 +1,10 @@
+package com.ipartek.formacion.ejemplo.logicanegocio;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class LogicaNegocioException extends RuntimeException {
+
+	private static final long serialVersionUID = -365959137350976817L;
+
+}
--- Ejemplo/src/main/java/com/ipartek/formacion/ejemplo/logicanegocio/PublicoNegocioImpl.java
+++ Ejemplo/src/main/java/com/ipartek/formacion/ejemplo/logicanegocio/PublicoNegocioImpl.java
@@ -4,12 +4,18 @@ import com.ipartek.formacion.ejemplo.accesodatos.ProductoDao;
 import com.ipartek.formacion.ejemplo.accesodatos.ProductoDaoSqlite;
 import com.ipartek.formacion.ejemplo.entidades.Producto;
 
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import lombok.extern.java.Log;
 
 @Log
 public class PublicoNegocioImpl implements PublicoNegocio {
 	private ProductoDao daoProducto = new ProductoDaoSqlite(); 
 	
+	private static ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+	private static Validator validator = factory.getValidator();
+
 	@Override
 	public Iterable<Producto> obtenerProductos() {
 		log.info("Se han pedido todos los productos");
@@ -19,6 +25,12 @@ public class PublicoNegocioImpl implements PublicoNegocio {
 	@Override
 	public Producto agregarProducto(Producto producto) {
 		log.info("Se va a insertar el producto " + producto);
+		
+		var errores = validator.validate(producto);
+		if(errores.size() > 0) {
+			throw new LogicaNegocioException("No se admiten productos sin validar");
+		}
+		
 		return daoProducto.insertar(producto);
 	}
 
