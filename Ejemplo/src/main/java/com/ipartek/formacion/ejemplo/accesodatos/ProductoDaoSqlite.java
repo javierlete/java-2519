a5308fa8722728921e7e2a3066971755052c2885
java <java@DESKTOP-MHK683Q.mshome.net>
2023-10-03 11:08:39 +0200

    Rellenado completo de funcionalidades del DAO

--- Ejemplo/src/main/java/com/ipartek/formacion/ejemplo/accesodatos/ProductoDaoSqlite.java
+++ Ejemplo/src/main/java/com/ipartek/formacion/ejemplo/accesodatos/ProductoDaoSqlite.java
@@ -16,8 +16,15 @@ public class ProductoDaoSqlite implements ProductoDao {
 	private static final String URL = "jdbc:sqlite:C:\\Users\\Java\\git\\java-2519\\Ejemplo\\sql\\ejemplo.db";
 	private static final String USER = null;
 	private static final String PASS = null;
+	
 	private static final String SQL_SELECT = "SELECT id, nombre, precio, fecha_caducidad FROM productos";
+	private static final String SQL_SELECT_ID = SQL_SELECT + " WHERE id=?";
+	private static final String SQL_SELECT_NOMBRE = SQL_SELECT + " WHERE nombre LIKE ?";
+	private static final String SQL_SELECT_PRECIOS = SQL_SELECT + " WHERE precio BETWEEN ? AND ?";
+
 	private static final String SQL_INSERT = "INSERT INTO productos (nombre, precio, fecha_caducidad) VALUES (?,?,?)";
+	private static final String SQL_UPDATE = "UPDATE productos SET nombre=?, precio=?, fecha_caducidad=? WHERE id=?";
+	private static final String SQL_DELETE = "DELETE FROM productos WHERE id=?";
 
 	static {
 		try {
@@ -61,7 +68,84 @@ public class ProductoDaoSqlite implements ProductoDao {
 
 	@Override
 	public Producto selectPorId(Long id) {
-		throw new AccesoDatosException("NO IMPLEMENTADO");
+		try (Connection con = obtenerConexion();
+				PreparedStatement pst = con.prepareStatement(SQL_SELECT_ID);
+				) {
+			pst.setLong(1, id);
+			
+			ResultSet rs = pst.executeQuery();
+			
+			Producto producto = null;
+
+			if (rs.next()) {
+				String fechaTexto = rs.getString("fecha_caducidad");
+				LocalDate fechaCaducidad = fechaTexto == null || fechaTexto.trim().length() == 0 ? null : LocalDate.parse(fechaTexto);			
+				producto = Producto.builder().id(rs.getLong("id")).nombre(rs.getString("nombre"))
+						.precio(rs.getBigDecimal("precio")).fechaCaducidad(fechaCaducidad)
+						.build();
+			}
+			
+			return producto;
+		} catch (SQLException e) {
+			throw new AccesoDatosException("Error al obtener el producto id " + id, e);
+		}
+	}
+
+	@Override
+	public Iterable<Producto> selectPorNombre(String nombre) {
+		try (Connection con = obtenerConexion();
+				PreparedStatement pst = con.prepareStatement(SQL_SELECT_NOMBRE);
+				) {
+			pst.setString(1, "%" + nombre + "%");
+			
+			ResultSet rs = pst.executeQuery();
+			
+			var productos = new ArrayList<Producto>();
+			Producto producto;
+
+			while (rs.next()) {
+				String fechaTexto = rs.getString("fecha_caducidad");
+				LocalDate fechaCaducidad = fechaTexto == null || fechaTexto.trim().length() == 0 ? null : LocalDate.parse(fechaTexto);			
+				producto = Producto.builder().id(rs.getLong("id")).nombre(rs.getString("nombre"))
+						.precio(rs.getBigDecimal("precio")).fechaCaducidad(fechaCaducidad)
+						.build();
+				
+				productos.add(producto);
+			}
+			
+			return productos;
+		} catch (SQLException e) {
+			throw new AccesoDatosException("Error al obtener los productos", e);
+		}
+	}
+
+	@Override
+	public Iterable<Producto> selectPorRangoPrecio(BigDecimal precioMinimo, BigDecimal precioMaximo) {
+		try (Connection con = obtenerConexion();
+				PreparedStatement pst = con.prepareStatement(SQL_SELECT_PRECIOS);
+				) {
+			pst.setBigDecimal(1, precioMinimo);
+			pst.setBigDecimal(2, precioMaximo);
+			
+			ResultSet rs = pst.executeQuery();
+			
+			var productos = new ArrayList<Producto>();
+			Producto producto;
+
+			while (rs.next()) {
+				String fechaTexto = rs.getString("fecha_caducidad");
+				LocalDate fechaCaducidad = fechaTexto == null || fechaTexto.trim().length() == 0 ? null : LocalDate.parse(fechaTexto);			
+				producto = Producto.builder().id(rs.getLong("id")).nombre(rs.getString("nombre"))
+						.precio(rs.getBigDecimal("precio")).fechaCaducidad(fechaCaducidad)
+						.build();
+				
+				productos.add(producto);
+			}
+			
+			return productos;
+		} catch (SQLException e) {
+			throw new AccesoDatosException("Error al obtener los productos", e);
+		}
 	}
 
 	@Override
@@ -87,22 +171,38 @@ public class ProductoDaoSqlite implements ProductoDao {
 
 	@Override
 	public Producto modificar(Producto producto) {
-		throw new AccesoDatosException("NO IMPLEMENTADO");
+		try (Connection con = obtenerConexion();
+				PreparedStatement pst = con.prepareStatement(SQL_UPDATE)) {
+
+			pst.setString(1, producto.getNombre());
+			pst.setBigDecimal(2, producto.getPrecio());
+			
+			LocalDate fechaCaducidad = producto.getFechaCaducidad();
+			String fechaCaducidadString = fechaCaducidad == null ? null : fechaCaducidad.toString(); 
+			
+			pst.setString(3, fechaCaducidadString);
+			
+			pst.setLong(4, producto.getId());
+			
+			pst.executeUpdate();
+			
+			return producto;
+		} catch (SQLException e) {
+			throw new AccesoDatosException("Error al modificar el producto", e);
+		}
 	}
 
 	@Override
 	public void borrar(Long id) {
-		throw new AccesoDatosException("NO IMPLEMENTADO");
-	}
+		try (Connection con = obtenerConexion();
+				PreparedStatement pst = con.prepareStatement(SQL_DELETE)) {
 
-	@Override
-	public Iterable<Producto> selectPorNombre(String nombre) {
-		throw new AccesoDatosException("NO IMPLEMENTADO");
-	}
-
-	@Override
-	public Iterable<Producto> selectPorRangoPrecio(BigDecimal precioMinimo, BigDecimal precioMaximo) {
-		throw new AccesoDatosException("NO IMPLEMENTADO");
+			pst.setLong(1, id);
+			
+			pst.executeUpdate();
+		} catch (SQLException e) {
+			throw new AccesoDatosException("Error al borrar el producto", e);
+		}
 	}
 
 }
