44e958b111234923133140695d9b355a3876d4df
java <java@DESKTOP-MHK683Q.mshome.net>
2023-10-03 11:09:03 +0200

    Completamos el negocio con todas las operaciones

--- Ejemplo/src/main/java/com/ipartek/formacion/ejemplo/logicanegocio/PublicoNegocio.java
+++ Ejemplo/src/main/java/com/ipartek/formacion/ejemplo/logicanegocio/PublicoNegocio.java
@@ -1,8 +1,16 @@
 package com.ipartek.formacion.ejemplo.logicanegocio;
 
+import java.math.BigDecimal;
+
 import com.ipartek.formacion.ejemplo.entidades.Producto;
 
 public interface PublicoNegocio {
 	public Iterable<Producto> obtenerProductos();
+	public Producto obtenerProductoPorId(Long id);
+	public Iterable<Producto> obtenerProductosPorNombre(String nombre);
+	public Iterable<Producto> obtenerProductosPorPrecio(BigDecimal minimo, BigDecimal maximo);
+	
 	public Producto agregarProducto(Producto producto);
+	public Producto modificarProducto(Producto producto);
+	public void eliminarProducto(Long id);
 }
